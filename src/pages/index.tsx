3a49fc719d43ed7aa24e0557a720a8c55595177e
marcelldac <dacmarcell@gmail.com>
2023-10-23 15:13:10 -0300

    chore: add react fragment to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import LoginForm from "@/components/LoginForm";
+import { Box, Switch, useColorMode } from "@chakra-ui/react";
 
 export default function Home() {
-  return <LoginForm />;
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 }
