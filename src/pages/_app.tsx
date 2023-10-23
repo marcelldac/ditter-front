95d05ae2fc3aa19748878a7941b75178a1a0c5cb
marcelldac <dacmarcell@gmail.com>
2023-10-23 15:02:09 -0300

    chore: add session and chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "153e75",
+    700: "2a69ac",
+  },
+};
+
+export const theme = extendTheme({ colors });
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
