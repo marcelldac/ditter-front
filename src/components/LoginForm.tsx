ce11eeb19d96afc2c5ddc82abb2994e1260bafb7
marcelldac <dacmarcell@gmail.com>
2023-10-23 15:12:50 -0300

    feat: add toggle theme switch

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,19 +1,23 @@
 import {
+  Box,
   Button,
   Flex,
   Heading,
   Input,
-  useColorMode,
   Switch,
+  useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
 
 export default function LoginForm() {
-  const { toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue("gray.100", "gray.700");
+  const { toggleColorMode } = useColorMode();
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
       <Flex direction="column" background={formBackground} p={12} rounded={6}>
+        <Box textAlign="right">
+          <Switch onChange={toggleColorMode} />
+        </Box>
         <Heading mb={6}>Login</Heading>
         <Input
           placeholder="johndoe@email.com"
@@ -28,7 +32,6 @@ export default function LoginForm() {
           type="password"
         />
         <Button colorScheme="teal">Login</Button>
-        <Switch onChange={toggleColorMode} />
       </Flex>
     </Flex>
   );
