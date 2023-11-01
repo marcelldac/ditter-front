7f1b003b999aaae8c109e9f50f320eb377213637
marcelldac <dacmarcell@gmail.com>
2023-11-01 11:16:54 -0300

    refactor: use RegisterForm

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,16 +1,13 @@
 import {
   Box,
-  Button,
   Flex,
   Heading,
-  Input,
   Switch,
   useColorMode,
   useColorModeValue,
-  Checkbox,
-  Link,
-  Text,
 } from "@chakra-ui/react";
+
+import RegisterForm from "@/components/RegisterForm";
 
 export default function Register() {
   const formBackground = useColorModeValue("gray.100", "gray.700");
@@ -23,28 +20,7 @@ export default function Register() {
           <Switch onChange={toggleColorMode} />
         </Box>
         <Heading mb={6}>Register</Heading>
-        <Input placeholder="Your e-mail" variant="filled" mb={3} type="email" />
-        <Input
-          placeholder="Your password"
-          variant="filled"
-          mb={3}
-          type="password"
-          required
-        />
-        <Input
-          placeholder="Confirm password"
-          variant="filled"
-          type="password"
-          required
-        />
-        <Flex direction="row" color="gray.500" my={8}>
-          <Checkbox size="md" colorScheme="teal" />
-          <Text>Concordo com os termos... blablabla</Text>
-        </Flex>
-        <Button colorScheme="teal">Register</Button>
-        <Box textAlign="center" mt={5} color="gray.500">
-          <Link href="/">Already have an account?</Link>
-        </Box>
+        <RegisterForm />
       </Flex>
     </Flex>
   );
