0cfbe91d589fd7d6cda16727b3b5fb64ceafa868
marcelldac <dacmarcell@gmail.com>
2023-11-04 16:34:06 -0300

    feat: add login page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import LoginForm from "@/components/LoginForm";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Login - System</title>
+      </Head>
       <LoginForm />
     </>
   );
