f658b851b71d49c1b4dee16ddf1830b0ac9b692d
marcelldac <dacmarcell@gmail.com>
2023-11-04 16:35:13 -0300

    feat: create home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home - System</title>
+      </Head>
+      <h1>Home</h1>
+    </>
+  );
+}
