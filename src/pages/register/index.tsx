54798cef047799ae8e2057303340fefcf4f89daf
marcelldac <dacmarcell@gmail.com>
2023-11-04 16:34:23 -0300

    feat: add register page title

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -8,20 +8,27 @@ import {
 } from "@chakra-ui/react";
 
 import RegisterForm from "@/components/RegisterForm";
+import Head from "next/head";
 
 export default function Register() {
   const formBackground = useColorModeValue("gray.100", "gray.700");
   const { toggleColorMode } = useColorMode();
 
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex direction="column" background={formBackground} p={12} rounded={6}>
-        <Box textAlign="right">
-          <Switch onChange={toggleColorMode} />
-        </Box>
-        <Heading mb={6}>Register</Heading>
-        <RegisterForm />
+    <>
+      <Head>
+        <title>Register - System</title>
+      </Head>
+
+      <Flex height="100vh" alignItems="center" justifyContent="center">
+        <Flex direction="column" background={formBackground} p={12} rounded={6}>
+          <Box textAlign="right">
+            <Switch onChange={toggleColorMode} />
+          </Box>
+          <Heading mb={6}>Register</Heading>
+          <RegisterForm />
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
