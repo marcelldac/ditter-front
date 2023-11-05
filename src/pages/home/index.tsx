392e7ebb91bffd5de873f69cc3963c13b82aae3f
marcelldac <dacmarcell@gmail.com>
2023-11-04 22:27:59 -0300

    feat: add authentication

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,18 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { useContext, useEffect } from "react";
+import { parseCookies } from "nookies";
+import { AuthContext } from "@/contexts/AuthContext";
+import { api } from "@/services/api";
+import { getAPIClient } from "@/services/axios";
 
 export default function Home() {
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get("/users");
+  }, []);
+
   return (
     <>
       <Head>
@@ -10,3 +22,23 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apiClient = getAPIClient(ctx);
+  const { ["nextauth.token"]: token } = parseCookies(ctx);
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  await apiClient.get("/users");
+
+  return {
+    props: {},
+  };
+};
