4ae564aee5610782abc739f6db22f555e5abe697
marcelldac <dacmarcell@gmail.com>
2023-11-08 16:45:42 -0300

    style: comments api token

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,21 +1,21 @@
 import axios from "axios";
-import { parseCookies } from "nookies";
+/* import { parseCookies } from "nookies"; */
 
 export function getAPIClient(ctx?: any) {
-  const { "nextauth.token": token } = parseCookies(ctx);
+  /* const { "nextauth.token": token } = parseCookies(ctx); */
 
   const api = axios.create({
     baseURL: "http://localhost:4000/api",
   });
 
-  api.interceptors.request.use((config) => {
+  /* api.interceptors.request.use((config) => {
     console.log(config);
     return config;
   });
 
   if (token) {
     api.defaults.headers["Authorization"] = `Bearer ${token}`;
-  }
+  } */
 
   return api;
 }
